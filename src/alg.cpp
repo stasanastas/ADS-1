561326d0dbf3e1309b5f5cf6ca82a10178a07060
Анастасия Карова <76094090+stasanastas@users.noreply.github.com>
2021-03-24 23:36:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,36 +1,36 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int i, j, lastIndex; 
-  i =size−1; 
-  while (i>0) {
-    lastIndex=0; 
-    for (j=0; j < i; j++) 
-      if( arr [ j +1] < arr [ j ]) {
-        swap (& arr [ j +1] ,& arr [ j ]);
-        lastIndex = j;
-      } 
-    i = lastIndex; 
+  int *arr = new int[size];
+  int x;
+  for (int i = 0; i < size - 1; i++) {
+    for (int j = 0; i < size - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        x = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = x;
+      }
+    }
   }
-  int l=arr, r=size-1, n=0, res=0;
-  while (l<=r){
-    int c=(l+r)/2;
-    if (arr[c]>value)
-      r=arr[c];
-    else l=arr[c];
-    if(arr[c]==value){
+  int l = arr, r = size - 1, n = 0, res = 0;
+  while (l <= r) {
+    int c = (l + r) / 2;
+    if (arr[c] > value)
+      r = arr[c];
+    else l = arr[c];
+    if(arr[c] == value) {
       res++;
-      while((arr[c-n]==value) or (arr[c+n]==value)){
-        if (arr[c-n]==value){
+      while((arr[c - n] == value) || (arr[c + n] == value)) {
+        if (arr[c - n] == value) {
           res++;
           n++;
         }
-        if (arr[c+n]==value){
+        if (arr[c + n] == value) {
           res++;
           n++;
         }
       }
     }
     //printf(res);
-    return res; // если ничего не найдено
+    return res;
 }
